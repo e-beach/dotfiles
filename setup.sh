f70d9d12358f4e15f9cf4ec60ce6d4011f125cf7
Elliott Beach <elliott2.71828@gmail.com>
2017-07-17 22:23:14 -0500

    fix things up

--- setup.sh
+++ setup.sh
@@ -4,35 +4,39 @@
 # Overwrite dotfiles in home directory with symlinks to the dofiles in this git repo.
 #
 
-# Exit immediatly on any error, as we could delete important stuff.
-set -e
 
-# `readlink -f` does not work on OSX.
-# see http://stackoverflow.com/questions/1055671/how-can-i-get-the-behavior-of-gnus-readlink-f-on-a-mac
-DOTFILES="/Users/elliott/me/projects/dotfiles"
-
-echo $DOTFILES
-cd $DOTFILES
-
-function doIt() {
-
-    for f in \
-        .aliases .bash_prompt .bashrc .curlrc .editorconfig .exports .extras .functions .gitconfig .gitignore .hushlogin .screenrc .vimrc .wgetrc
-    do
-        dest="$HOME/$f"
-        src="$DOTFILES/$f"
-        ln -f -s "$src" "$dest"
-    done
-
-    # install vim plugins and vimrc
-    bash vimInstall.sh
-}
-
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-    doIt;
+if [ -z "$1" ] ; then 
+    bash "$0" `pwd` ;
 else
-    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        doIt;
-    fi;
+
+	# Exit immediatly on any error, as we could delete important stuff.
+	set -e
+
+	DOTFILES="$1"
+
+	echo "dofiles directory: " $DOTFILES
+	cd $DOTFILES
+
+	function doIt() {
+
+	    for f in \
+		.aliases .bash_prompt .bashrc .curlrc .editorconfig .exports .extras .functions .gitconfig .gitignore .hushlogin .screenrc .vimrc .wgetrc
+	    do
+		dest="$HOME/$f"
+		src="$DOTFILES/$f"
+		ln -f -s "$src" "$dest"
+	    done
+
+	    # install vim plugins and vimrc
+	    bash vimInstall.sh
+	}
+
+	if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	    doIt;
+	else
+	    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	    if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt;
+	    fi;
+	fi;
 fi;
